0baad47c61c6a1321fdebd09898fd0ad96aa874e
신형섭 <brothersup@kakao.com>
2022-03-01 23:07:59 +0900

    6단계 푸는중

--- src/main/java/study/algorithm/level6/Main3.java
+++ src/main/java/study/algorithm/level6/Main3.java
@@ -1,15 +1,41 @@
 package study.algorithm.level6;
 
+import java.io.*;
+
 public class Main3 {
+    boolean isHansu(int num) {
+        boolean result = true;
+        String[] numbers = String.valueOf(num).split("");
+        if (numbers.length >= 3) {
+            int d = Integer.parseInt(numbers[0]) - Integer.parseInt(numbers[1]);
+            for (int i = 1; i < numbers.length; i++) {
+                if (i != numbers.length - 1 && d != Integer.parseInt(numbers[i]) - Integer.parseInt(numbers[i + 1])) {
+                    result = false;
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        // 1, 2, 3, 4, 5, 6, 7, 8, 9 => 9개
-        // 10, 12, 13, 14, 15, 16, 17, 18, 19 => 9개
-        // 20, 21, 23, 24, 25, 26, 27, 28, 29 => 9개
-        // 30, 31, 32, 34, 35, 36, 37, 38, 39 => 9개
-        // 40, 41, 42, 43, 45, 46, 47, 48, 49 => 9개
-        // 50, 51, 52, 53, 54, 56, 57, 58, 59 => 9개
-        // ...
-        // 90, 91, 92, 93, 94, 95, 96, 97, 98 => 9개  ===> 90개
-        // 100, 101, 102, 103, 104, 105, 106, 107, 108, 109
+        Main3 main = new Main3();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        try {
+            int input = Integer.parseInt(br.readLine());
+            br.close();
+
+            int cnt = 0;
+            for (int i = 1; i <= input; i++) {
+                if (main.isHansu(i)) {
+                    cnt++;
+                };
+            }
+            bw.write(String.valueOf(cnt));
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
